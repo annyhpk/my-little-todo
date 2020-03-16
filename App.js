f6ec67e033ce74b993cd88da77368de18783b062
Chanho Kim <cksgh063@gmail.com>
2020-03-16 23:28:52 +0900

    Codinf the UI part One

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet,
+  Text, 
+  View, 
+  StatusBar, 
+  Dimensions, 
+  TextInput,
+  Platform
+ } from 'react-native';
+
+const {height, width} = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar barStyle="light-content" />
+      <Text style={styles.title}>My Little Todo</Text>
+      <View style={styles.card}> 
+        <TextInput style={styles.input} placeholder={"New TODO"} />
+      </View>
     </View>
   );
 }
@@ -12,8 +26,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#5fcf6a',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    flex: 1,
+    color: "white",
+    fontSize: 30,
+    marginTop: 50,
+    fontWeight: "200",
+    marginBottom: -230
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 2,
+    width: width - 35,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor:"rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset:{
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 5
+      }
+    })
+  }
 });
